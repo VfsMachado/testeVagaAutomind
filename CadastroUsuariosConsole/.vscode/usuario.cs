dd536db5e899e6a2026ff69ce452626d4746c4e3
Victor Fernando Silva Machado Evangelista <vfsmachado27@gmail.com>
2025-03-18 15:56:14 -0300

    CLASSE USUARIO FEITA
    classe usuário criada e suas propriedades

--- CadastroUsuariosConsole/.vscode/usuario.cs
+++ CadastroUsuariosConsole/.vscode/usuario.cs
@@ -3,4 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace CadastroUsuariosConsole
-{ 
+{  
+// classe para o usuario 
+public class Usuario {
+    public string NOME { get; set; }
+    public string EMAIL { get; set; }
+    public int IDADE { get; set; }
+}
+
+}
+
+
+class Program
+{ }
